6070e0d5ebf7d4b3a4573e640888a21bbb6abc81
Prachi Patel <165886307+AwesomePrachi@users.noreply.github.com>
2025-12-12 14:00:27 +0530

    initial haveit full-stack project
    Updated text and button styles for better clarity and design.

--- client/src/components/ConfirmBox.jsx
+++ client/src/components/ConfirmBox.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { IoClose } from "react-icons/io5";
 
-const ConfirmBox = ({cancel,confirm,close}) => {
+const ConfirmBox = ({cancel, confirm, close}) => {
   return (
     <div className='fixed inset-0 z-50 bg-neutral-600/70 p-4 flex justify-center items-center'>
       <div className='bg-white w-full max-w-md p-4 rounded'>
            <div className='flex justify-between items-center gap-3'>
-                <h1 className='font-semibold'>Permanent Delete</h1>
+                <h1 className='font-semibold'>Delete Permanently</h1>
                 <button onClick={close}>
                     <IoClose size={25} />
                 </button>
            </div>
-           <p className='my-4'>Are you sure permanent delete ?</p>
-           <div className='w-fit ml-auto flex items-center gap-3'>
-                <button onClick={cancel} className='px-4 py-1 border rounded border-red-500 text-red-500 hover:bg-red-500 hover:text-white'>Cancel</button>
-                <button onClick={confirm} className='px-4 py-1 border rounded border-green-600 text-green-600 hover:bg-green-600 hover:text-white'>Confirm</button>
+           <p className='my-4'>Are you sure you want to delete this permanently?</p>
+           <div className='w-fit ml-auto flex items-center gap-8'>
+                <button onClick={cancel} className='px-6.5 py-1.5 text-black/45  border-2 border-black/20 rounded'>Cancel</button>
+                <button onClick={confirm} className='px-6 py-2 text-white bg-green-700 hover:bg-green-600 rounded'>Confirm</button>
            </div>
       </div>
     </div>
